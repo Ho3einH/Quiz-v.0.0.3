e8eab04642d2f9a914934673f7402b680d3497aa
S.Hosein Hoseini <s.hosein.h.82@gmail.com>
2025-04-17 16:35:16 +0330

    fifth commit (resolve sixth problem)

--- poetry.js
+++ poetry.js
@@ -1,35 +1,35 @@
 'use strict';
+const poems = {
+  a: 'به هر کجا که روم با تو آشنا باشم\nاگر دل از تو بگیرم کجا روم ای دوست',
+  b: 'فلک را سقف بشکافیم و طرحی نو دراندازیم\nبیا تا گل بر افشانیم و می در  ساغر اندازیم',
+  c: 'پاییز آمد و برگ از درختان ریخت\nدل بی قرار من باز به یادت گریخت',
+  d: 'تو را من چشم در راهم شباهنگام\nکه می‌گیرند در شاخ تلاجن سایه‌ها رنگ سیاهی',
+  e: 'هر بلائی کز تو آید نعمتی است\nهرکه را رنجی دهی آن راحتی است',
+  f: 'زان به تاریکی گذاری بنده را\nتا ببیند آن رخ تابنده را',
+  g: 'محتسب، مستی به ره دید و گریبانش گرفت\nمست گفت: ای دوست، این پیراهن است، افسار نیست',
+  h: 'گفت: مستی، زان سبب افتان و خیزان می روی\nگفت: جرم راه رفتن نیست، ره هموار نیست',
+  i: 'که شادی و غم گیتی نمیکنند دوام\nز غم مباش غمین و مشو ز شادی شاد',
+  j: 'چون پر کاه بی سر و سامان نمی‌شود\nآنکس که همنشین خرد شد، ز هر نسیم',
+};
 export default function poetry(verse) {
   let result = null;
-  const poems = {
-    a: 'اگر دل از تو بگیرم کجا روم ای دوست؟\nبه هر کجا که روم با تو آشنا باشم',
-    b: 'بیا تا گل بر افشانیم و می در ساغر اندازیم\nفلک را سقف بشکافیم و طرحی نو دراندازیم',
-    c: 'پاییز آمد و برگ از درختان ریخت\nدل بی‌قرار من باز به یادت گریخت',
-    d: 'تو را من چشم در راهم شباهنگام\nکه می‌گیرند در شاخ تلاجن سایه‌ها رنگ سیاهی',
-  };
 
-  verse = verse.trim();
-  const stanza = verse.split('\n');
-  let lastLetter = verse[verse.length - 1];
+  verse = verse.trim().split('\n').reverse().join(' / ');
+  const lastLetter = verse.charAt(verse.length - 1);
 
-  if (!poems[lastLetter]) {
-    if (stanza.length > 1) {
-      lastLetter = stanza[stanza.length - 1].slice(-1);
-    }
-    if (!poems[lastLetter] && stanza.length > 0) {
-      lastLetter = stanza[0].slice(-1);
-    }
+  console.log(`-----------------------------------------------------`);
+
+  for (const [index, el] of Object.entries(poems)) {
+    const firstLetter = el.charAt(0);
+    // console.log(el);
+    // console.log(lastLetter);
+    // console.log(firstLetter);
+    (firstLetter[0] || firstLetter[1]) === lastLetter ? (result = el) : null;
   }
 
-  result = poems[lastLetter] || poems['a'];
-  console.log(result);
-  return null;
+  return result;
 }
 
 console.log(`-------------------------- 6 ---------------------------`);
 
-console.log(
-  poetry(
-    'اگر دل از تو بگیرم کجا روم ای دوست؟\nبه هر کجا که روم با تو آشنا باشم'
-  )
-);
+console.log(poetry(poems.a));
